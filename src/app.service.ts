84b8d8cdb354b55c51d3b6b6c5059c255d3be579
QuadriniL <quadrinho.tiolu@gmail.com>
2023-02-17 22:30:44 -0300

    feat: return github status and set a developer tier

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,49 @@
+import * as handlebars from 'handlebars';
 import { Injectable } from '@nestjs/common';
+import * as fs from 'fs';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  template = fs.readFileSync('src/views/stats.hbs', 'utf8');
+
+  async getStats(): Promise<string> {
+    const githubStats = await fetch(
+      'https://api.github.com/search/commits?q=author:QuadriniL',
+    ).then((res) => res.json());
+
+    const gitProfileRepos = await fetch(
+      'https://api.github.com/users/QuadriniL/repos',
+    ).then((res) => res.json());
+
+    const stars = gitProfileRepos.reduce(
+      (acc, repo) => acc + repo.stargazers_count,
+      0,
+    );
+
+    const languages = gitProfileRepos.map((repo) => repo.language);
+
+    for (let i = 0; i < languages.length; i++) {
+      if (languages[i] === null) {
+        languages.splice(i, 1);
+      }
+    }
+
+    const languagesWithCount = languages.reduce((acc, language) => {
+      if (acc[language]) {
+        acc[language] += 1;
+      } else {
+        acc[language] = 1;
+      }
+      return acc;
+    }, {});
+
+    const commits = githubStats.total_count;
+
+    const compiled = handlebars.compile(this.template);
+    return compiled({
+      commits,
+      stars,
+      languages: Object.keys(languagesWithCount).length,
+    });
   }
 }
